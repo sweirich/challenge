9184504749ee71c3fab37ab26a582d716ade8a89
Stephanie Weirich <sweirich@cis.upenn.edu>
2020-06-20 15:59:37 -0400

    make examples uniform

--- debruijn/src/PolyTypedOpt.hs
+++ debruijn/src/PolyTypedOpt.hs
@@ -2,10 +2,9 @@
 module PolyTypedOpt where
 
 import Imports
--- Note: bug in singletons. Cannot qualify this module import.
-import Subst
+import qualified Subst as W
 import SubstProperties 
-import qualified SubstTypedOpt as T
+import qualified SubstTypedOpt as S
 
 -- We import the definition of types from the "untyped" AST so that we can
 -- later write a type checker that shares these types. (See TypeCheck.hs).
@@ -16,11 +15,11 @@ data Exp :: [Ty] -> Ty -> Type where
   IntE   :: Int
          -> Exp g IntTy
 
-  VarE   :: T.Idx g t
+  VarE   :: S.Idx g t
          -> Exp g t
 
-  LamE   :: Π t1               -- type of binder
-         -> T.Bind Exp t1 g t2    -- body of abstraction
+  LamE   :: Π t1                  -- type of binder
+         -> S.Bind Exp t1 g t2    -- body of abstraction
          -> Exp g (t1 :-> t2)
 
   AppE   :: Exp g (t1 :-> t2)     -- function
@@ -32,32 +31,81 @@ data Exp :: [Ty] -> Ty -> Type where
 
   TyApp  :: Exp g (PolyTy t1)     -- type function
          -> Π t2               -- type argument
-         -> Exp g (Subst (SingleSub t2) t1)
+         -> Exp g (W.Subst (W.SingleSub t2) t1)
 
-instance T.SubstDB Exp where
+instance S.SubstDB Exp where
    var = VarE
 
    subst s (IntE x)     = IntE x
-   subst s (VarE x)     = T.applySub s x
-   subst s (LamE ty e)  = LamE ty (T.substBind s e)
-   subst s (AppE e1 e2) = AppE (T.subst s e1) (T.subst s e2)
-   subst s (TyLam e)    = TyLam (T.subst (substSubTy sIncSub s) e)
-   subst s (TyApp e t)  = TyApp (T.subst s e) t
+   subst s (VarE x)     = S.applySub s x
+   subst s (LamE ty e)  = LamE ty (S.substBind s e)
+   subst s (AppE e1 e2) = AppE (S.subst s e1) (S.subst s e2)
+   subst s (TyLam e)    = TyLam (S.subst (incTy W.sIncSub s) e)
+   subst s (TyApp e t)  = TyApp (S.subst s e) t
 
 substTy :: forall g s ty.
-   Π (s :: Sub Ty) -> Exp g ty -> Exp (Map (SubstSym1 s) g) (Subst s ty)
+   Π (s :: W.Sub Ty) -> Exp g ty -> Exp (Map (W.SubstSym1 s) g) (W.Subst s ty)
 substTy s (IntE x)     = IntE x
-substTy s (VarE n)     = VarE $ T.mapIdx @(SubstSym1 s)  n
-substTy s (LamE t e)   = LamE (sSubst s t) (T.bind (substTy s (T.unbind e)))
+substTy s (VarE n)     = VarE $ S.mapIdx @(W.SubstSym1 s)  n
+substTy s (LamE t e)   = LamE (W.sSubst s t) (S.bind (substTy s (S.unbind e)))
 substTy s (AppE e1 e2) = AppE (substTy s e1) (substTy s e2)
 substTy s (TyLam (e :: Exp (IncList g) t))    
-    | Refl <- axiom1 @g s = TyLam (substTy (sLift s) e)
+    | Refl <- axiom1 @g s = TyLam (substTy (W.sLift s) e)
 substTy s (TyApp (e :: Exp g (PolyTy t1)) (t :: STy t2))  
     | Refl <- axiom2 @t1 @t2 s
-                       = TyApp (substTy s e) (sSubst s t)
+                       = TyApp (substTy s e) (W.sSubst s t)
 
-substSubTy :: forall s g g'. Π s -> T.Sub Exp g g' -> T.Sub Exp (Map (SubstSym1 s) g) (Map (SubstSym1 s) g')
-substSubTy s (T.Inc (x :: T.IncBy g1)) 
-  | Refl <- axiom_map1 @(SubstSym1 s) @g1 @g = T.Inc (T.mapInc @(SubstSym1 s)  x) 
-substSubTy s (e T.:< s1)   = substTy s e T.:< substSubTy s s1
-substSubTy s (s1 T.:<> s2) = substSubTy s s1 T.:<> substSubTy s s2
+incTy :: forall s g g'. Π s -> S.Sub Exp g g' -> S.Sub Exp (Map (W.SubstSym1 s) g) (Map (W.SubstSym1 s) g')
+incTy s (S.Inc (x :: S.IncBy g1)) 
+  | Refl <- axiom_map1 @(W.SubstSym1 s) @g1 @g = S.Inc (S.mapInc @(W.SubstSym1 s)  x) 
+incTy s (e S.:< s1)   = substTy s e S.:< incTy s s1
+incTy s (s1 S.:<> s2) = incTy s s1 S.:<> incTy s s2
+
+
+-------------------------------------------------------------------
+
+-- | Small-step evaluation of closed terms
+step :: Exp '[] t -> Maybe (Exp '[] t)
+step (IntE x)     = Nothing
+step (VarE n)     = case n of {}  
+step (LamE t e)   = Nothing
+step (AppE e1 e2) = Just $ stepApp e1 e2
+step (TyLam e)    = Nothing
+step (TyApp e t)  = Just $ stepTyApp e t
+
+-- | Helper function for the AppE case. This function proves that we will
+-- *always* take a step if a closed term is an application expression.
+stepApp :: Exp '[] (t1 :-> t2) -> Exp '[] t1  -> Exp '[] t2
+--stepApp (IntE x)       e2 = error "Type error"
+stepApp (VarE n)       e2 = case n of {}
+stepApp (LamE t e1)    e2 = S.instantiate e1 e2
+stepApp (AppE e1' e2') e2 = AppE (stepApp e1' e2') e2
+--stepApp (TyLam e)      e2 = error "Type error"
+stepApp (TyApp e1 t1)  e2 = AppE (stepTyApp e1 t1) e2
+
+-- | Helper function for the TyApp case. This function proves that we will
+-- *always* take a step if a closed term is a type application expression.
+stepTyApp :: Exp '[] (PolyTy t1) -> Π t -> Exp '[] (W.Subst (W.SingleSub t) t1)
+--stepTyApp (IntE x)       e2 = error "Type error"
+stepTyApp (VarE n)       t1 = case n of {}
+--stepTyApp (LamE t e1)    t1 = error "Type error"
+stepTyApp (AppE e1' e2') t1 = TyApp (stepApp e1' e2') t1
+stepTyApp (TyLam e1)     t1 = substTy (W.sSingleSub t1) e1
+stepTyApp (TyApp e1 t2)  t1 = TyApp (stepTyApp e1 t2) t1
+
+-- | open reduction
+reduce :: forall g t. Exp g t -> Exp g t
+reduce (IntE x)   = IntE x
+reduce (VarE n)   = VarE n
+reduce (LamE t e) = LamE t (S.bind (reduce (S.unbind e)))
+reduce (AppE (LamE t e1) e2) = S.subst (S.single (reduce e2)) (reduce (S.unbind e1))
+--reduce (AppE (IntE x)     e2) = error "Type error"
+--reduce (AppE (TyLam e1)   e2) = error "Type error"
+reduce (AppE e1 e2)          = AppE (reduce e1) (reduce e2)
+reduce (TyLam e)             = TyLam (reduce e)
+reduce (TyApp (TyLam e) (t1 :: STy t1))
+      | Refl <- axiom6 @t1 @g
+      = substTy (W.sSingleSub t1) (reduce e)
+--reduce (TyApp (IntE x)     t) = error "Type error"
+--reduce (TyApp (LamE t1 e2) t) = error "Type error"    
+reduce (TyApp e t)           = TyApp (reduce e) t
--- debruijn/src/SimpleTypedOpt.hs
+++ debruijn/src/SimpleTypedOpt.hs
@@ -17,15 +17,13 @@ data Exp :: [Ty] -> Ty -> Type where
  VarE   :: Idx g t               -- variable index
         -> Exp g t
 
- LamE   :: Π t1               -- type of binder
-        -> Bind Exp t1 g t2    -- body of abstraction
+ LamE   :: Π t1                  -- type of binder
+        -> Bind Exp t1 g t2      -- body of abstraction
         -> Exp g (t1 :-> t2)
 
  AppE   :: Exp g (t1 :-> t2)     -- function
         -> Exp g t1              -- argument
         -> Exp g t2
-
- SubE   :: Sub Exp g g' -> Exp g t -> Exp g' t
  
 instance SubstDB Exp where
    var = VarE
@@ -34,60 +32,36 @@ instance SubstDB Exp where
    subst s (VarE x)     = applySub s x
    subst s (LamE ty e)  = LamE ty (substBind s e)
    subst s (AppE e1 e2) = AppE (subst s e1) (subst s e2)
-   subst s (SubE s1 e)  = SubE (comp s1 s) e
 
 
 -----------------------------------------------------------------------
--- | is an expression a value?
-value :: Exp g t -> Bool
-value (IntE x)   = True
-value (LamE t e) = True
-value _          = False
+-- Examples
 
--- | Small-step evaluation
+-- | Small-step evaluation of closed terms.
+-- 
+-- Either return the next term or Nothing, if the term is already a value.
+-- Note that the type of this function shows that types are preserved during
+-- evaluation
 step :: Exp '[] t -> Maybe (Exp '[] t)
-step (IntE x)   = Nothing
-step (VarE n)   = case n of {}
-step (LamE t e) = Nothing
-step (AppE (LamE t e1) e2)   = Just $ instantiate e1 e2
---step (AppE (IntE x) e2)      = error "Type error!"
-step (AppE e1 e2) = do e1' <- step e1
-                       return $ AppE e1' e2
-step (SubE s1 e) = step (subst s1 e)                       
+step (IntE x)     = Nothing
+step (VarE n)     = case n of {}
+step (LamE t e)   = Nothing
+step (AppE e1 e2) = Just $ stepApp e1 e2 where
 
--- | open reduction
+    -- Helper function for the AppE case. This function "proves" that we will
+    -- *always* take a step if a closed term is an application expression.
+    stepApp :: Exp '[] (t1 :-> t2) -> Exp '[] t1  -> Exp '[] t2
+    --stepApp (IntE x)       e2 = error "Type error"
+    stepApp (VarE n)       e2 = case n of {}    
+    stepApp (LamE t e1)    e2 = instantiate e1 e2
+    stepApp (AppE e1' e2') e2 = AppE (stepApp e1' e2') e2
+
+
+-- | Reduce open expressions to their normal form
 reduce :: Exp g t -> Exp g t
 reduce (IntE x)   = IntE x
 reduce (VarE n)   = VarE n
-reduce (LamE t e) = LamE t (bind (reduce (unbind e))) 
-reduce (AppE (LamE t e1) e2)   = reduce (instantiate e1 e2)  -- not the same as parallel reduction?!?
-reduce (AppE e1 e2) | value e1 = error "Type error!"
+reduce (LamE t e) = LamE t (bind (reduce (unbind e)))
+reduce (AppE (LamE t e1) e2)   = subst (single (reduce e2)) (reduce (unbind e1))
+-- reduce (AppE (IntE x)    e2)   = error "Type error!"
 reduce (AppE e1 e2) = AppE (reduce e1) (reduce e2)
-reduce (SubE s e) = reduce (subst s e)
-
--- | A proof of False
-data False where
-   IsFalse :: (forall a . a) -> False
-
-eval :: Exp '[] t -> Exp '[] t
-eval (AppE e1 e2) = 
-   case unLam (eval e1) of
-      Left (IsFalse k) -> k
-      Right e11        -> subst (single e2) e11
-eval (IntE x)     = IntE x
-eval (VarE v)     = case v of {}
-eval (LamE ty e)  = LamE ty e
-eval (SubE s e)   = eval (subst s e)
-
--- | The only error allowed is if we don't evaluate the term
--- completely. Maybe this example would be better as a small-step
--- semantics.
-
-unLam :: Exp '[] (t1 :-> t2) -> Either False (Exp '[t1] t2)
-unLam (LamE t1 e11) = Right (unbind e11)
-unLam (AppE e1 e2)  = case unLam e1 of
-                        Left i -> Left i
-                        Right _ -> error "Found a non-value"
-unLam (VarE v)      = Left (IsFalse (case v of {}))
-unLam (SubE s e)    = unLam (subst s e)
-
